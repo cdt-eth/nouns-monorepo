6ac27e65d3d9bf3a2935158315aa6e73aa424ccf
christianturner <christianturner@hey.com>
2021-12-12 20:57:40 -0500

    hide Documentation on homepage

--- packages/nouns-webapp/src/pages/Auction/index.tsx
+++ packages/nouns-webapp/src/pages/Auction/index.tsx
@@ -1,7 +1,7 @@
 import { BigNumber } from 'ethers';
 import Banner from '../../components/Banner';
 import Auction from '../../components/Auction';
-import Documentation from '../../components/Documentation';
+// import Documentation from '../../components/Documentation';
 import HistoryCollection from '../../components/HistoryCollection';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { setOnDisplayAuctionNounId } from '../../state/slices/onDisplayAuction';
@@ -50,7 +50,7 @@ const AuctionPage: React.FC<AuctionPageProps> = props => {
       {lastAuctionNounId && (
         <HistoryCollection latestNounId={BigNumber.from(lastAuctionNounId)} historyCount={10} />
       )}
-      <Documentation />
+      {/* <Documentation /> */}
     </>
   );
 };
