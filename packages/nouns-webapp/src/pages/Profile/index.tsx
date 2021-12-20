bf69812903ef0826b93183629e7675c984fbb26a
christianturner <christianturner@hey.com>
2021-12-20 17:00:49 -0500

    comment

--- packages/nouns-webapp/src/pages/Profile/index.tsx
+++ packages/nouns-webapp/src/pages/Profile/index.tsx
@@ -21,7 +21,7 @@ const ProfilePage: React.FC<ProfilePageProps> = props => {
 
   const dispatch = useAppDispatch();
   const lastAuctionNounId = useAppSelector(state => state.onDisplayAuction.lastAuctionNounId);
-  let stateBgColor = useAppSelector(state => state.application.stateBackgroundColor);
+  // let stateBgColor = useAppSelector(state => state.application.stateBackgroundColor);
 
   const loadedNounHandler = (seed: INounSeed) => {
     dispatch(setStateBackgroundColor(seed.background === 0 ? grey : beige));
