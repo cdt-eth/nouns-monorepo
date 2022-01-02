b4f7e9bf96ccfb0990bf0d020cd1c1f5f8e5e85e
christianturner <christianturner@hey.com>
2022-01-02 07:48:08 -0500

    rndm fx

--- packages/nouns-webapp/src/components/Banner/index.tsx
+++ packages/nouns-webapp/src/components/Banner/index.tsx
@@ -3,13 +3,84 @@ import Section from '../../layout/Section';
 import noundersArt from '../../assets/nounders_art2.png';
 import noundersDev from '../../assets/nounders_dev.png';
 import mug from '../../assets/mug.png';
+import Link from '../Link';
+
 import snake from '../../assets/highlight/snake.png';
 import phone from '../../assets/highlight/phone2.png';
 import alien from '../../assets/highlight/alien.png';
 import log from '../../assets/highlight/log.png';
-// import log from '../../assets/highlight/log2.png';
-import Link from '../Link';
-// import { Col } from 'react-bootstrap';
+import clown from '../../assets/highlight/clown.png';
+import orange from '../../assets/highlight/orange.png';
+import cinderblock from '../../assets/highlight/cinderblock.png';
+import diamond from '../../assets/highlight/diamond.png';
+import Quotes from '../Quotes';
+
+const quoteData = [
+  {
+    name: 'Gremplin',
+    quotes: [
+      {
+        noun: snake,
+        quote:
+          "The snake was an attempt bringing more animal heads into the fold without being strictly a head. Leaving a gap in the heads typically doesn't feel good, so I tried a tighter coil here. I think ultimately it was just stylistically not a great fit.",
+      },
+      {
+        noun: phone,
+        quote:
+          "The cordless phone eventually found its way in as a more straightforward rectangle, but this one is much closer to one I had myself. I gravitate toward the nostalgic nouns even though nouns are a bridge to the future. This one's major flaw is that it leaves shoulder corners exposed, which is weird for us.",
+      },
+    ],
+  },
+  {
+    name: 'Timpers',
+    quotes: [
+      {
+        noun: alien,
+        quote: 'Alien quote.',
+      },
+      {
+        noun: log,
+        quote: 'Log quote.',
+      },
+    ],
+  },
+  {
+    name: 'eBoy',
+    quotes: [
+      {
+        noun: clown,
+        quote: 'Clown quote.',
+      },
+      {
+        noun: orange,
+        quote: 'Orange quote.',
+      },
+    ],
+  },
+  {
+    name: '4156',
+    quotes: [
+      {
+        noun: cinderblock,
+        quote: 'Cinderblock quote.',
+      },
+      {
+        noun: diamond,
+        quote: 'Diamond quote.',
+      },
+    ],
+  },
+];
+
+const getRandomIdx = () => {
+  var y = Math.random();
+
+  if (y < 0.5) {
+    return 0;
+  } else {
+    return 1;
+  }
+};
 
 const Banner = () => {
   const PlaygroundLink = <Link text="the Playground" url="/playground" leavesPage={false} />;
@@ -46,56 +117,6 @@ const Banner = () => {
           </div>
         </div>
 
-        {/* OLD */}
-        {/* OLD */}
-        {/* OLD */}
-        {/* OLD */}
-        {/* Gremplin Quotes */}
-        {/* <h1 className="font-nouns text-left text-5xl quotes">Gremplin</h1>
-      <div className={classes.wrapperGrid}>
-        <div className={classes.quote}>
-          <img className="gr" src={snake} alt="snake" />
-          <p className={classes.copy}>
-            "The snake was an attempt bringing more animal heads into the fold without being
-            strictly a head. Leaving a gap in the heads typically doesn't feel good, so I tried a
-            tighter coil here. I think ultimately it was just stylistically not a great fit."
-          </p>
-        </div>
-
-        <div className={classes.quote}>
-          <img className="gr" src={phone} alt="phone" />
-          <p className={classes.copy}>
-            "The cordless phone eventually found its way in as a more straightforward rectangle, but
-            this one is much closer to one I had myself. I gravitate toward the nostalgic nouns even
-            though nouns are a bridge to the future. This one's major flaw is that it leaves
-            shoulder corners exposed, which is weird for us."
-          </p>
-        </div>
-      </div> */}
-
-        {/* Timpers Quotes */}
-        {/* <h1 className="font-nouns text-left text-5xl quotes">Timpers</h1>
-      <div className={classes.wrapperGrid}>
-        <div className={classes.quote}>
-          <img className="gr" src={alien} alt="snake" />
-          <p className={classes.copy}>
-            "The snake was an attempt bringing more animal heads into the fold without being
-            strictly a head. Leaving a gap in the heads typically doesn't feel good, so I tried a
-            tighter coil here. I think ultimately it was just stylistically not a great fit."
-          </p>
-        </div>
-
-        <div className={classes.quote}>
-          <img className="gr" src={log} alt="phone" />
-          <p className={classes.copy}>
-            "The cordless phone eventually found its way in as a more straightforward rectangle, but
-            this one is much closer to one I had myself. I gravitate toward the nostalgic nouns even
-            though nouns are a bridge to the future. This one's major flaw is that it leaves
-            shoulder corners exposed, which is weird for us."
-          </p>
-        </div>
-      </div> */}
-
         <div className={classes.spacerDiv}></div>
 
         {/* Dev Nounders */}
@@ -109,73 +130,13 @@ const Banner = () => {
         </div>
       </Section>
       <Section fullWidth={true} className={classes.bannerSection}>
-        {/* NEW */}
-        {/* NEW */}
-        {/* NEW */}
-        {/* NEW */}
-        {/* Gremplin Quotes */}
-
         <div className={classes.both}>
           <div className="container-lg container-lg2">
             <p className={classes.notes}>Nounder's Notes</p>
             <div className={classes.wrap}>
-              <div className={classes.wrapperGrid2}>
-                <div className={classes.quote2}>
-                  <img className="gr" src={snake} alt="snake" />
-                  <p className={classes.copy}>
-                    "The snake was an attempt bringing more animal heads into the fold without being
-                    strictly a head. Leaving a gap in the heads typically doesn't feel good, so I
-                    tried a tighter coil here. I think ultimately it was just stylistically not a
-                    great fit."
-                    <br />
-                    <span className={classes.credit}>Gremplin</span>
-                  </p>
-                </div>
-              </div>
-              <div className={classes.wrapperGrid2}>
-                <div className={classes.quote2}>
-                  <img className="gr" src={phone} alt="snake" />
-                  <p className={classes.copy}>
-                    "The cordless phone eventually found its way in as a more straightforward
-                    rectangle, but this one is much closer to one I had myself. I gravitate toward
-                    the nostalgic nouns even though nouns are a bridge to the future. This one's
-                    major flaw is that it leaves shoulder corners exposed, which is weird for us."
-                    <br />
-                    <span className={classes.credit}>Timpers</span>
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className={classes.spacerDiv}></div>
-
-            <div className={classes.wrap}>
-              <div className={classes.wrapperGrid2}>
-                <div className={classes.quote2}>
-                  <img className="gr" src={alien} alt="snake" />
-                  <p className={classes.copy}>
-                    "The snake was an attempt bringing more animal heads into the fold without being
-                    strictly a head. Leaving a gap in the heads typically doesn't feel good, so I
-                    tried a tighter coil here. I think ultimately it was just stylistically not a
-                    great fit."
-                    <br />
-                    <span className={classes.credit}>4156</span>
-                  </p>
-                </div>
-              </div>
-              <div className={classes.wrapperGrid2}>
-                <div className={classes.quote2}>
-                  <img className="gr" src={log} alt="snake" />
-                  <p className={classes.copy}>
-                    "The cordless phone eventually found its way in as a more straightforward
-                    rectangle, but this one is much closer to one I had myself. I gravitate toward
-                    the nostalgic nouns even though nouns are a bridge to the future. This one's
-                    major flaw is that it leaves shoulder corners exposed, which is weird for us."
-                    <br />
-                    <span className={classes.credit}>eBoy</span>
-                  </p>
-                </div>
-              </div>
+              {quoteData.map(nounder => (
+                <Quotes name={nounder.name} quote={nounder.quotes[getRandomIdx()]} />
+              ))}
             </div>
           </div>
         </div>
---
+++ packages/nouns-webapp/src/components/Quotes/index.tsx
@@ -0,0 +1,24 @@
+import classes from './Quotes.module.css';
+
+interface QuotesProps {
+  name: string;
+  quote: any;
+}
+
+const Quotes = ({ name, quote }: QuotesProps) => {
+  console.log('quote', quote);
+  return (
+    <div className={classes.wrapperGrid2}>
+      <div className={classes.quote2}>
+        <img className="gr" src={quote.noun} alt="snake" />
+        <p className={classes.copy}>
+          {quote.quote}
+          <br />
+          <span className={classes.credit}>{name}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Quotes;
